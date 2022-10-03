19dc526309f8cbb2d074ea93631fc66c10b21169
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 07:58:23 +0100

    Dependency Injection added.

--- ECommerce.Api.Orders/Startup.cs
+++ ECommerce.Api.Orders/Startup.cs
@@ -1,6 +1,10 @@
+using ECommerce.Api.Orders.Db;
+using ECommerce.Api.Orders.Interfaces;
+using ECommerce.Api.Orders.Providers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +28,13 @@ namespace ECommerce.Api.Orders
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<OrdersDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("Orders");
+            });
+            services.AddScoped<IOrdersProvider, OrdersProvider>();
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddControllers();
         }
 
