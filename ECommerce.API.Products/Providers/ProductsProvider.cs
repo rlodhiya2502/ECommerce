bd0f9d891962478068f811fefa00aac50f516069
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 07:21:47 +0100

    Db context generated, provider class referenced.

---
+++ ECommerce.API.Products/Providers/ProductsProvider.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Interfaces;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Providers
+{
+    public class ProductsProvider : IProductsProvider
+    {
+        private readonly ProductsDbContext dbContext;
+        private readonly ILogger<ProductsProvider> logger;
+        private readonly IMapper mapper;
+
+        public ProductsProvider(ProductsDbContext dbContext, ILogger<ProductsProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+            this.logger = logger;
+        }
+
+        public Task<(bool IsSuccess, Models.Product Product, string ErrorMessage)> GetProductAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<(bool IsSuccess, IEnumerable<Models.Product> Products, string ErrorMessage)> GetProductsAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
