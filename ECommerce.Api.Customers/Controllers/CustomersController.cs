e8ebebbb9519080fcc7132b1e61c6ce6d46c97e4
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 06:16:54 +0100

    Generate interface, model and its basic scaffolding.

---
+++ ECommerce.Api.Customers/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Api.Customers.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Customers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomersProvider customersProvider;
+
+        public CustomersController(ICustomersProvider customersProvider)
+        {
+            this.customersProvider = customersProvider;
+        }
+
+        //TODO: GET CUSTOMERS 
+
+        //TODO: GET CUSTOMERS BY ID
+
+    }
+}
