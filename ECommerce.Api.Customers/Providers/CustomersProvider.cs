4f773db3b454404a3b39066e253ee64acc937fa7
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 06:31:59 +0100

    Add Entityframework references and base implementation of customer provider

---
+++ ECommerce.Api.Customers/Providers/CustomersProvider.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Api.Customers.Interfaces;
+using ECommerce.Api.Customers.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Customers.Providers
+{
+    public class CustomersProvider : ICustomersProvider
+    {
+        public Task<(bool IsSuccess, Customer Customer, string ErrorMessage)> GetCustomerAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<(bool IsSuccess, IEnumerable<Customer> Customers, string ErrorMessage)> GetCustomersAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
