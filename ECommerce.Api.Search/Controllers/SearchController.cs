eda633c108ce6f96b48ab3a0e1363953c185a871
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 08:34:39 +0100

    Wire Customer Service with http client.

--- ECommerce.Api.Search/Controllers/SearchController.cs
+++ ECommerce.Api.Search/Controllers/SearchController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace ECommerce.Api.Search.Controllers
 {
@@ -7,5 +10,21 @@ namespace ECommerce.Api.Search.Controllers
     [ApiController]
     public class SearchController : ControllerBase
     {
+        private readonly ISearchService searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            this.searchService = searchService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> SearchAsync(SearchTerm term)
+        {
+            var result = await searchService.SearchAsync(term.CustomerId);
+            if (result.IsSuccess)
+            {
+                return Ok(result.SearchResults);
+            }
+            return NotFound();
+        }
     }
 }
---
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.Api.Search.Interfaces;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Search.Services
+{
+    public class SearchService : ISearchService
+    {
+        private readonly ICustomersService customersService;
+        public SearchService(ICustomersService customersService)
+        {
+            this.customersService = customersService;
+
+        }
+        public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerId)
+        {
+            var customersResult = await customersService.GetCustomerAsync(customerId);
+
+            var result = new
+            {
+                Customer = customersResult.IsSuccess ?
+                                customersResult.Customer :
+                                new { Name = "Customer information is not available" }
+            };
+
+            return (true, result);
+
+        }
+    }
+}
