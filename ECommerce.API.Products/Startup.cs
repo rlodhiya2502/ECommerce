bb207576b98700b71cbf82f851dafd3ac2c52f31
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 07:26:05 +0100

    Configure Automapper

--- ECommerce.API.Products/Startup.cs
+++ ECommerce.API.Products/Startup.cs
@@ -31,7 +31,7 @@ namespace ECommerce.API.Products
             services.AddScoped<IProductsProvider, IProductsProvider>();
             services.AddDbContext<ProductsDbContext>(options =>            
                 options.UseInMemoryDatabase("Products"));
-            
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
