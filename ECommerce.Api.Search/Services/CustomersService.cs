3ecd433aa0882b5b2a42a61b71576a7770040df2
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 08:24:25 +0100

    Implementing CustomersService

---
+++ ECommerce.Api.Search/Services/CustomersService.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Api.Search.Interfaces;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Search.Services
+{
+    public class CustomersService : ICustomersService
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly ILogger<CustomersService> logger;
+
+        public CustomersService(IHttpClientFactory httpClientFactory, ILogger<CustomersService> logger)
+        {
+            this.httpClientFactory = httpClientFactory;
+            this.logger = logger;
+        }
+        public Task<(bool IsSuccess, dynamic Customer, string ErrorMessage)> GetCustomerAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- ECommerce.Api.Search/Startup.cs
+++ ECommerce.Api.Search/Startup.cs
@@ -1,3 +1,5 @@
+using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +26,12 @@ namespace ECommerce.Api.Search
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ICustomersService, CustomersService>();
+            services.AddHttpClient("CustomersService", config =>
+            {
+                config.BaseAddress = new Uri(Configuration["Services:Customers"]);
+            });
+
             services.AddControllers();
         }
 
