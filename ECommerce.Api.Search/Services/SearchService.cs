0b6c8c5d8f5258cd5204b2fda4acb47b89d88fbd
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 10:42:14 +0100

    Refine search with order and item with client id and product name.

--- ECommerce.Api.Search/Services/SearchService.cs
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerce.Api.Search.Interfaces;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ECommerce.Api.Search.Services
@@ -22,14 +23,28 @@ namespace ECommerce.Api.Search.Services
             var ordersResult = await ordersService.GetOrdersAsync(customerId);
             var productsResult = await productsService.GetProductsAsync();
 
-            var result = new
+            if (ordersResult.IsSuccess)
             {
-                Customer = customersResult.IsSuccess ?
+                foreach (var orders in ordersResult.Orders)
+                {
+                    foreach (var item in orders.Items)
+                    {
+                        item.ProductName = productsResult.IsSuccess ?
+                            productsResult.Products.FirstOrDefault(p => p.Id == item.ProductId)?.Name :
+                            "Product information is not available";
+                    }
+                }
+                var result = new
+                {
+                    Customer = customersResult.IsSuccess ?
                                 customersResult.Customer :
-                                new { Name = "Customer information is not available" }
-            };
+                                new { Name = "Customer information is not available" },
+                    Orders = ordersResult.Orders
+                };
 
-            return (true, result);
+                return (true, result);
+            }
+            return (false, null);
 
         }
     }
