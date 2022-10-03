b878d18dbdd979871c00805ebea40264fbd93e71
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 07:57:31 +0100

    Db Context and Provider with seed data.

--- ECommerce.Api.Orders/Db/OrdersDbContext.cs
+++ ECommerce.Api.Orders/Db/OrdersDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace ECommerce.Api.Orders.Db
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Api.Orders.Db
 {
-    public class OrdersDbContext
+    public class OrdersDbContext : DbContext
     {
+        public DbSet<Order> Orders { get; set; }
+        public OrdersDbContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
--- ECommerce.Api.Orders/Providers/OrdersProvider.cs
+++ ECommerce.Api.Orders/Providers/OrdersProvider.cs
@@ -1,6 +1,89 @@
-﻿namespace ECommerce.Api.Orders.Providers
+﻿using AutoMapper;
+using ECommerce.Api.Orders.Db;
+using ECommerce.Api.Orders.Interfaces;
+using ECommerce.Api.Orders.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Order = ECommerce.Api.Orders.Db.Order;
+using OrderItem = ECommerce.Api.Orders.Db.OrderItem;
+
+namespace ECommerce.Api.Orders.Providers
 {
-    public class OrdersProvider
+    public class OrdersProvider : IOrdersProvider
     {
+        private readonly OrdersDbContext dbContext;
+        private readonly ILogger<OrdersProvider> logger;
+        private readonly IMapper mapper;
+
+        public OrdersProvider(OrdersDbContext dbContext, ILogger<OrdersProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            if (!dbContext.Orders.Any())
+            {
+                dbContext.Orders.Add(new Order()
+                {
+                    Id = 1,
+                    CustomerId = 1,
+                    OrderDate = DateTime.Now,
+                    Items = new List<OrderItem>()
+                    {
+                        new OrderItem() { OrderId = 1, ProductId = 1, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 1, ProductId = 2, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 1, ProductId = 3, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 2, ProductId = 2, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 3, ProductId = 3, Quantity = 1, UnitPrice = 100 }
+                    },
+                    Total = 100
+                });
+                dbContext.Orders.Add(new Order()
+                {
+                    Id = 2,
+                    CustomerId = 1,
+                    OrderDate = DateTime.Now.AddDays(-1),
+                    Items = new List<OrderItem>()
+                    {
+                        new OrderItem() { OrderId = 1, ProductId = 1, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 1, ProductId = 2, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 1, ProductId = 3, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 2, ProductId = 2, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 3, ProductId = 3, Quantity = 1, UnitPrice = 100 }
+                    },
+                    Total = 100
+                });
+                dbContext.Orders.Add(new Order()
+                {
+                    Id = 3,
+                    CustomerId = 2,
+                    OrderDate = DateTime.Now,
+                    Items = new List<OrderItem>()
+                    {
+                        new OrderItem() { OrderId = 1, ProductId = 1, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 2, ProductId = 2, Quantity = 10, UnitPrice = 10 },
+                        new OrderItem() { OrderId = 3, ProductId = 3, Quantity = 1, UnitPrice = 100 }
+                    },
+                    Total = 100
+                });
+                dbContext.SaveChanges();
+            }
+        }
+        public Task<(bool IsSuccess, IEnumerable<Order> Orders, string ErrorMessage)> GetOrdersAsync(int customerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        Task<(bool IsSuccess, IEnumerable<Models.Order> Orders, string ErrorMessage)> IOrdersProvider.GetOrdersAsync(int customerId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
