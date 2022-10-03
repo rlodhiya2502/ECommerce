481292f5634d93f0aa731a124c3b0206dea8734c
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 07:29:07 +0100

    Add controller

---
+++ ECommerce.API.Products/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.Api.Products.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Products.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsProvider productsProvider;
+
+        public ProductsController(IProductsProvider productsProvider)
+        {
+            this.productsProvider = productsProvider;
+        }
+
+        //todo: getproducts
+        //todo: get products by id.
+    }
+}
