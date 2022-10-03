d65712506b21b2d3af16650172520a0e8782d505
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 07:24:29 +0100

    wire dependency injections

--- ECommerce.API.Products/Startup.cs
+++ ECommerce.API.Products/Startup.cs
@@ -1,6 +1,9 @@
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +28,10 @@ namespace ECommerce.API.Products
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IProductsProvider, IProductsProvider>();
+            services.AddDbContext<ProductsDbContext>(options =>            
+                options.UseInMemoryDatabase("Products"));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
