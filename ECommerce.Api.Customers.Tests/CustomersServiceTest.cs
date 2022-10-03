df8313cd862c5ab22ed928f981fc323aa3d58b5c
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-10-03 10:58:51 +0100

    Adding test projects

---
+++ ECommerce.Api.Customers.Tests/CustomersServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ECommerce.Api.Customers.Tests
+{
+    public class CustomersServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
